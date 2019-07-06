5a0d79496db125985a1f5df3d3fc657fea4e8648
Will Robinson <wrobinson91@gmail.com>
2019-07-06 15:20:22 -0400

    removed node mods cache, updated schema controller

--- server/controllers/schemaController.js
+++ server/controllers/schemaController.js
@@ -2,25 +2,27 @@ const pool = require('./database');
 
 const schemaController = {
   createSchema: (req, res, next) => {
-    // extract all the form inputs
+    // extract all the form inputs. not there yet
     const { schemaName } = req.body;
 
-    // create the table
+    // check if table has already been made
     pool.query(
       // have to add more options
-      `CREATE TABLE IF NOT EXISTS ${schemaname}(key VARCHAR(50), type VARCHAR(50), options boolean, required boolean, default_value VARCHAR(50), user_id INT)`,
+      'CREATE TABLE IF NOT EXISTS Schemas (user_id INT, schema_name VARCHAR (50), schema_id INT, key VARCHAR(50), type VARCHAR(50), options, BOOLEAN, unique BOOLEAN);',
       (err, result) => {
         if (err) return console.error(err);
+
+        // add to table once it has been created
         console.log('CREATE TABLE schema', result);
 
         // populate the table
         // iterate thru keys to create rows in the table
         pool.query(
-          `INSERT INTO ${schemaname} (key, type, options, required, default_value, user_id) values ($1, $2, $3, $4, $5, $6)`
+          `INSERT INTO ${schemaName} (user_id, schema_name, schema_id, key, type, options, unique) values ($1, $2, $3, $4, $5, $6, $7)`,
         );
-      }
+      },
     );
-  }
+  },
 };
 
 module.exports = schemaController;
