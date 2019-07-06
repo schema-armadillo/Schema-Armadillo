4bb37c939f67581bec287a1744e978992e3dfc3f
Khalid Umar <khalid.umar101@gmail.com>
2019-07-05 20:14:33 -0400

    webpack file config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        publicPath: '/build/',
+        hot: true,
+        port: 8080,
+        proxy: {
+            '/api/': 'http://localhost:3000',
+            '/auth/': 'http://localhost:3000'
+          }
+    },
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                ]
+            }
+        ]
+    }
+
+}
