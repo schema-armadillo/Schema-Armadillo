4e508dcfa1037c4ca1dda97e6f8da3af1b375117
Sophie <sophie.nye@gmail.com>
2019-07-07 11:58:19 -0400

    more styling for login page

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,6 @@ import ".././styles/App.css";
 import { hot } from "react-hot-loader";
 import { Route, Link, BrowserRouter as Router, Redirect } from "react-router-dom";
 import Login from "./Login"
-import Signup from "./Signup"
 import Dashboard from "./Dashboard"
 
 
@@ -18,7 +17,7 @@ class App extends Component {
             <Router>
                 <Route exact path="/" component={Login} />
                 <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
+                <Route path="/signup" component={Login} />
                 <Route path="/dashboard" component={Dashboard} />
                 <Route path="/myschema" />
             </Router>
