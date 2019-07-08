9f58cc691c7245bae492a65b1a5a4e34de63a2f2
Will Robinson <wrobinson91@gmail.com>
2019-07-07 20:43:01 -0400

    checkout before going to dev

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
-import ".././styles/Login.css";
-import { hot } from "react-hot-loader";
-import { Route, Link, BrowserRouter as Router, Redirect } from "react-router-dom";
+import React, { Component } from 'react';
+import '../styles/Login.css';
+import { hot } from 'react-hot-loader';
+import {
+  Route, Link, BrowserRouter as Router, Redirect,
+} from 'react-router-dom';
 import styled from 'styled-components';
-import armadillo from '../Armadillo-icon.jpg'
+import armadillo from '../Armadillo-icon.jpg';
 
 const Form = styled.form`
     display: flex;
@@ -11,84 +13,82 @@ const Form = styled.form`
     height: 450px;
     padding-top: 150px;
     align-items: center;
-    `
+    `;
 
 const Input = styled.input`
     width: 375px;
     height: 50px;
     margin: 30px;
     padding: 10px;
-`
+`;
 
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-
-        this.handleChangeEmail = this.handleChangeEmail.bind(this);
-        this.handleChangePassword = this.handleChangePassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
 
-    handleChangeEmail(event) {
-        console.log(event.target.value)
-        this.setState({ email: event.target.value });
-    }
+  handleChangeEmail(event) {
+    console.log(event.target.value);
+    this.setState({ email: event.target.value });
+  }
 
-    handleChangePassword(event) {
-        this.setState({ password: event.target.value });
-    }
+  handleChangePassword(event) {
+    this.setState({ password: event.target.value });
+  }
 
-    handleSubmit(event) {
-        console.log('A login was submitted: ' + this.state.email);
-        console.log('A login was submitted: ' + this.state.password);
-        event.preventDefault();
-        fetch('/test', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state)
-        })
-            .then(data => data.json())
-            .then(obj => {
-                console.log(obj)
-                this.history.pushState(null, obj.redirecturl)
-            })
-            .catch(err => console.log('login fetch err ', err))
-    }
+  handleSubmit(event) {
+    console.log(`A login was submitted: ${this.state.email}`);
+    console.log(`A login was submitted: ${this.state.password}`);
+    event.preventDefault();
+    // changed this to create acct, to test
+    fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then(data => data.json())
+      .then((obj) => {
+        console.log(obj);
+        // this.history.pushState(null, obj.redirecturl);
+      })
+      .catch(err => console.log('login fetch err ', err));
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <div>
-                    <Form className="signupForm" onSubmit={this.handleSubmit}>
-                        <h1 className="signup">Sign up</h1>
-                        <input className="entry emailField" type="text" placeholder="email" />
-                        <input className="entry passwordField" type="password" placeholder="password" />
-                        <input className="signupButton" type="submit" value="Yeehaw!" />
-                    </Form>
-                </div>
-                <div>
-                    <Form className="loginForm" onSubmit={this.handleSubmit}>
-                        {/* <img className="armadillo" src={armadillo} alt="armadillo logo" /> */}
-                        <h1>Log in to Schema Armadillo</h1>
-                        <input className="entry emailField" type="text" placeholder="email" value={this.state.email} onChange={this.handleChangeEmail} />
-                        <input className="entry passwordField" type="password" placeholder="password" value={this.state.password} onChange={this.handleChangePassword} />
-                        <input className="loginButton" type="submit" value="Giddy-up!" />
-                    </Form>
-                </div>
-            </div >
-        );
-    }
-
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <Form className="signupForm" onSubmit={this.handleSubmit}>
+            <h1 className="signup">Sign up</h1>
+            <input className="entry emailField" type="text" placeholder="email" />
+            <input className="entry passwordField" type="password" placeholder="password" />
+            <input className="signupButton" type="submit" value="Yeehaw!" />
+          </Form>
+        </div>
+        <div>
+          <Form className="loginForm" onSubmit={this.handleSubmit}>
+            {/* <img className="armadillo" src={armadillo} alt="armadillo logo" /> */}
+            <h1>Log in to Schema Armadillo</h1>
+            <input className="entry emailField" type="text" placeholder="email" value={this.state.email} onChange={this.handleChangeEmail} />
+            <input className="entry passwordField" type="password" placeholder="password" value={this.state.password} onChange={this.handleChangePassword} />
+            <input className="loginButton" type="submit" value="Giddy-up!" />
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,70 +1,62 @@
-const pool = require('./database');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const pool = require('./database');
 
 
 const userController = {
   createUser: (req, res, next) => {
-    const { username, password } = req.body
+    const { username, password } = req.body;
     bcrypt.hash(password, 10, (err, hashResponse) => {
       if (err) {
         return res.status(500).send('error encrypting pw');
-      } else {
-        res.locals.username = username
-        res.locals.password = hashResponse
-        return next();
       }
+      res.locals.username = username;
+      res.locals.password = hashResponse;
+      return next();
+    });
+  },
+  addUserToDB: (req, res) => pool.query(
+    `INSERT INTO users (username, password) VALUES ('${res.locals.username}', '${res.locals.password}') RETURNING user_id, username`,
+  )
+    .then((data) => {
+      console.log(data);
+      return res.status(200).json(data);
     })
-  },
-  addUserToDB: (req, res) => {
-    return pool.query(
-      `INSERT INTO users (username, password) VALUES ('${res.locals.username}', '${res.locals.password}') RETURNING user_id, username`)
-      .then((data) => {
-        console.log(data)
-        return res.status(200).json(data)
-      })
-      .catch(err => {
-        console.log('error adding user to DB')
-        return res.status(500).send('Error creating user. PLease try again.');
-      })
-  },
+    .catch((err) => {
+      console.log('error adding user to DB');
+      return res.status(500).send('Error creating user. PLease try again.');
+    }),
 
   login: (req, res) => {
-    const { username, password } = req.body
+    const { username, password } = req.body;
     pool.query(`SELECT * FROM users WHERE username = '${username}'`)
       .then((data) => {
-        console.log('data rows', data.rows[0])
-        return data.rows[0]
+        console.log('data rows', data.rows[0]);
+        return data.rows[0];
       })
       .then((userFound) => {
-        console.log('user has been found')
+        console.log('user has been found');
         bcrypt.compare(password, userFound.password, (err, result) => {
           if (err) {
-            return res.send(err)
+            return res.send(err);
           }
           if (result) {
-             res.cookie('new', createToken(result))
-            return res.status(200).send('success')
+            res.cookie('new', createToken(result));
+            return res.status(200).send('success');
           }
-          else{
-            console.log('there is an error')
-            return res.status(500).send('Error')
-          }
-        }) 
+
+          console.log('there is an error');
+          return res.status(500).send('Error');
+        });
       })
-      .catch((err) => res.status(500).send(err))
+      .catch(err => res.status(500).send(err));
   },
 
 };
 
 const createToken = (user) => {
-  jwt.sign({ user }, 'secretkey', { expiresIn: 60 * 60 }, (err, token) => {
-    return token
-  })
-}
-
-
+  jwt.sign({ user }, 'secretkey', { expiresIn: 60 * 60 }, (err, token) => token);
+};
 
 
 module.exports = userController;
-
