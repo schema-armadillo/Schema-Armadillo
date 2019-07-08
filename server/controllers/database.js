e5ce4ba3a7526b58dcd0c71a352aa8aecdb04d32
Khalid Umar <khalid.umar101@gmail.com>
2019-07-08 11:28:50 -0400

    backend for create user, login work. updated table to make usernames a unique entryCo-authored by: Will <wrobinson91@gmail.com>Co-authored by: Khalid <khalid.umar101@gmail.com>Co-authored by: Sophie <sophie.nye@gmail.com>

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -9,8 +9,9 @@ const pool = new Pool({
 });
 
 // !!!!!! IMPORTANT: PW NEEDS TO BE LONGER
+// !!! IMPORTANT, USERNAME NEEDS TO BE UNIQUE
 pool.query(
-  'CREATE TABLE IF NOT EXISTS users(user_id SERIAL PRIMARY KEY, username VARCHAR(50), password VARCHAR(100), team_id INT)',
+  'CREATE TABLE IF NOT EXISTS users(user_id SERIAL PRIMARY KEY, username VARCHAR(50) UNIQUE, password VARCHAR(100), team_id INT)',
   (err, result) => {
     if (err) return console.error(err);
     console.log('CREATE TABLE users', result);
