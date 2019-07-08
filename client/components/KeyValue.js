5304f3f3dc41f4474c97245c01171d8f9d0a379a
Josh Kim <joshua940308@gmail.com>
2019-07-07 23:01:59 -0400

    reverted back to eb6b6b38fc8d45e0f3fce0f252d2e1863f008ff6

--- client/components/KeyValue.js
+++ client/components/KeyValue.js
@@ -1,82 +1,105 @@
-import React, { Component } from "react";
-import ".././styles/Dashboard.css";
-import { hot } from "react-hot-loader";
-import { Route, Link, BrowserRouter as Router, Redirect } from "react-router-dom";
-import styled from 'styled-components'
+import React, { Component } from 'react';
+import '.././styles/KeyValue.css';
+import { hot } from 'react-hot-loader';
+import {
+  Route,
+  Link,
+  BrowserRouter as Router,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
 import Select from 'react-select';
 
 const typeOptions = [
-    { value: 'string', label: 'String' },
-    { value: 'number', label: 'Number' },
-    { value: 'date', label: 'Date' },
-    { value: 'buffer', label: 'Buffer' },
-    { value: 'boolean', label: 'Boolean' },
-    { value: 'mixed', label: 'Mixed' },
-    { value: 'objectID', label: 'ObjectID' },
-    { value: 'array', label: 'Array' },
-    { value: 'decimal128', label: 'Decimal128' },
-    { value: 'map', label: 'Map' },
-]
+  { value: 'string', label: 'String' },
+  { value: 'number', label: 'Number' },
+  { value: 'date', label: 'Date' },
+  { value: 'buffer', label: 'Buffer' },
+  { value: 'boolean', label: 'Boolean' },
+  { value: 'mixed', label: 'Mixed' },
+  { value: 'objectID', label: 'ObjectID' },
+  { value: 'array', label: 'Array' },
+  { value: 'decimal128', label: 'Decimal128' },
+  { value: 'map', label: 'Map' }
+];
 
 class KeyValue extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedOption: null };
 
-    constructor(props) {
-        super(props);
-        this.state = { selectedOption: null };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeKey = this.handleChangeKey.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  handleChange(selectedOption) {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+  }
 
+  handleSubmit(event) {
+    console.log('state on submit ', this.state);
+    this.props.updateRow(
+      this.state.value,
+      this.state.selectedOption.label,
+      this.state.required
+    );
+    this.props.newRow();
+    console.log('handle submit ', this.props.rows);
+    event.preventDefault();
+  }
 
-    handleSubmit(event) {
-        console.log('A form was submitted:');
-        event.preventDefault();
-    }
+  handleChange(selectedOption) {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+  }
 
-    handleChange(selectedOption) {
-        this.setState({ selectedOption });
-        console.log(`Option selected:`, selectedOption);
-    };
+  handleChangeKey(event) {
+    this.setState({ value: event.target.value });
+  }
 
-    render() {
-        const { selectedOption } = this.state;
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input className="key" type="text" placeholder="key" />
-                    <Select
-                        value={selectedOption}
-                        onChange={this.handleChange}
-                        options={typeOptions}
-                        isClearable='true'
-                        isSearchable='true'
-                        closeMenuOnSelect='true'
-                    />
-                    {/* <select className="typeDrop">
-                        <option value="string">String</option>
-                        <option value="number">Number</option>
-                        <option value="date">Date</option>
-                        <option value="buffer">Buffer</option>
-                        <option value="boolean">Boolean</option>
-                        <option value="mixed">Mixed</option>
-                        <option value="objectID">ObjectID</option>
-                        <option value="array">Array</option>
-                        <option value="decimal128">Decimal128</option>
-                        <option value="map">Map</option>
-                    </select> */}
-                    <label>
-                        Required
-                        <input
-                            name="required"
-                            type="checkbox" />
-                    </label>
-                    <input className="save" type="submit" value="save key" />
-                </form>
-            </div>
-        );
-    }
+  handleInputChange(event) {
+    this.setState({ required: true });
+  }
 
+  render() {
+    console.log('rendering row ', this.props.rows);
+    const { selectedOption } = this.state;
+    return (
+      <div className='rowDiv'>
+        <form className='rowForm' onSubmit={this.handleSubmit}>
+          <input
+            className='key'
+            type='text'
+            placeholder='key'
+            value={this.state.value}
+            onChange={this.handleChangeKey}
+          />
+          <Select
+            className='select'
+            value={selectedOption}
+            onChange={this.handleChange}
+            options={typeOptions}
+            isClearable='true'
+            isSearchable='true'
+            closeMenuOnSelect='true'
+          />
+          <label>
+            Required
+            <input
+              name='required'
+              type='checkbox'
+              checked={this.state.required}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <input className='newRowButton' type='submit' value='new row' />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default KeyValue;
