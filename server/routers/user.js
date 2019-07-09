c45d35a2deda934263945ab675c2105e2c58a31d
Bryan Lee <Bryan_Lee@Bryans-MacBook-Pro.local>
2019-07-09 16:50:34 -0400

    deleted jwt/brypt/pool in user.js

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-
 const userController = require('../controllers/userController');
 const schemaController = require('../controllers/schemaController')
 
