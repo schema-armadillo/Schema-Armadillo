e7f4f6ac6708fbcfcb429b0bcedd66622d7ec77c
Dmitriyx <dmitriylevy01@gmail.com>
2019-07-09 16:24:30 -0400

    testing git

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const path = require('path');;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
