a083510d663d6b7d46d507a66ffc369357597286
Amanda Flink <amanda@Amandas-MacBook-Pro-2.local>
2019-07-09 18:42:27 -0400

    troubleshooting heroku

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const user = require('./routers/user');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
