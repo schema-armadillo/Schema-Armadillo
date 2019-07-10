4471f428534e4a0ca0103b38c94982496f08eeea
Ryan Dang <rydang@umich.edu>
2019-07-10 16:09:02 -0400

    formatted schemaname

--- client/components/SchemaName.js
+++ client/components/SchemaName.js
@@ -2,20 +2,17 @@ import React from 'react';
 
 
 function SchemaName(props) {
-
-    return (
-        <div className='schemaName'>
-            <input
-                type='text'
-                placeholder='Schema Name'
-                value={props.schemaName}
-                onChange={props.handleSchemaName}
-            />
-            <br />
-        </div>
-        
-    )
-
+  return (
+    <div className='schemaName'>
+      <input
+        type='text'
+        placeholder='Schema Name'
+        value={props.schemaName}
+        onChange={props.handleSchemaName}
+      />
+      <br />
+    </div>
+  );
 }
 
 export default SchemaName;
