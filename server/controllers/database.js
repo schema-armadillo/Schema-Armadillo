33a2230bfc3025d81a164bb9570c31e6c40732cd
Amanda Flink <amanda@Amandas-MacBook-Pro-2.local>
2019-07-10 11:19:07 -0400

    changed user table columns to add oauth

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 // !!!!!! IMPORTANT: PW NEEDS TO BE LONGER
 // !!! IMPORTANT, USERNAME NEEDS TO BE UNIQUE
 pool.query(
-  'CREATE TABLE IF NOT EXISTS users(user_id SERIAL PRIMARY KEY, username VARCHAR(50) UNIQUE, password VARCHAR(100), team_id INT)',
+  'CREATE TABLE IF NOT EXISTS users(user_id SERIAL PRIMARY KEY, username VARCHAR(50) UNIQUE, password VARCHAR(100), oauth VARCHAR(10))', //oauth will be null, github, or google
   (err, result) => {
     if (err) return console.error(err);
     console.log('CREATE TABLE users', result);
